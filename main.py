f070a60194ebf77c2627302e5c1e88ab1659eb42
Salavei <kano.mori@mail.ru>
2021-11-15 22:16:19 +0300

    transform parser -> def func, and selection variation city,transport, station, number_transport. main.py - add commands handling

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
+from parser import parser_data
 
-token = ' '
+token = '2114179429:AAGxSBcMwbFzxmPtYTtyIzmykrnkSCYhbqk'
 
 bot = Bot(token)
 dp = Dispatcher(bot)
 
-@dp.message_handler()
-async  def get_message(message: types.Message):
+
+@dp.message_handler(commands=['home'])
+async def get_message(message: types.Message):
+    trans_data = parser_data('minsk','autobus','24','Воронянского%20-%20ДС%20Зелёный%20Луг-6/Жуковского')
     chat_id = message.chat.id
-    text = "i'm bot"
+    text = f'От дома --- прошлый был в {trans_data[0]}, следующий в {trans_data[1]}'
     send_message = await bot.send_message(chat_id=chat_id, text=text)
     print(send_message.to_python())
 
 
-executor.start_polling(dp)
+@dp.message_handler(commands=['work'])
+async def get_message(message: types.Message):
+    trans_data = parser_data('minsk', 'autobus', '24', 'ДС%20Зелёный%20Луг-6%20-%20Воронянского/Романовская%20Слобода')
+    chat_id = message.chat.id
+    text = f'От работы --- прошлый был в {trans_data[0]}, следующий в {trans_data[1]}'
+    send_message = await bot.send_message(chat_id=chat_id, text=text)
+    print(send_message.to_python())
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
--- parser.py
+++ parser.py
@@ -2,17 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = 'https://kogda.by/routes/minsk/autobus/24/ДС%20Зелёный%20Луг-6%20-%20Воронянского/Жуковского'
-r = requests.get(url, headers = {'User-Agent': 'Mozilla/5.0'})
-html = r.text
-soup = BeautifulSoup(html, 'lxml')
-quote_boxes = soup.find_all('div', {"class":"content-block-desktop"})
+def parser_data(city,transport,number_transport,station):
+    url = f'https://kogda.by/routes/{city}/{transport}/{number_transport}/{station}'
+    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    html = r.text
+    soup = BeautifulSoup(html, 'lxml')
+    quote_boxes = soup.find_all('div', {"class": "content-block-desktop"})
 
-for q in quote_boxes:
-    soup2 = BeautifulSoup(str(q), 'lxml')
+    for q in quote_boxes:
+        soup2 = BeautifulSoup(str(q), 'lxml')
 
-    was = soup2.find('span', {"class":"passed"})
-    now = soup2.find('span', {"class":"future"})
-    will_be = soup2.find('span', {"class":"future"})
+        was = soup2.find('span', {"class": "passed"})
+        now = soup2.find('span', {"class": "future"})
+        will_be = soup2.find('span', {"class": "future"})
+        all_time = [' '.join(was.text.split()), ' '.join(now.text.split()), ' '.join(will_be.text.split())]
+    return all_time
 
-    print(' '.join(was.text.split()),' '.join(now.text.split()),' '.join(will_be.text.split()))
+parser_data('minsk','autobus','24','Воронянского%20-%20ДС%20Зелёный%20Луг-6/Жуковского')
