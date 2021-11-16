a83b99c52950c28af73f377a1005118fa5cd8763
Salavei <kano.mori@mail.ru>
2021-11-16 19:45:12 +0300

    make it better

--- parser.py
+++ parser.py
@@ -17,7 +17,7 @@ def parser_time_wait(city, transport, number_transport, station):
         will_be = soup2.find('span', {"class": "future"})
         all_time = [' '.join(was.text.split()), ' '.join(now.text.split()), ' '.join(will_be.text.split())]
         #добавить поиск 3-тьего столбика
-    return print(all_time)
+    return all_time
 parser_time_wait('minsk', 'autobus', '24', 'Воронянского%20-%20ДС%20Зелёный%20Луг-6/Жуковского')
 
 
@@ -36,21 +36,23 @@ def parser_station(city, transport, number_transport):
     for q in quote_boxes1:
         soup2 = BeautifulSoup(str(q), 'lxml')
         right = soup2.find('a').contents[0]
-    return print(' '.join(left.text.split()),' '.join(right.text.split()), sep='\n')
+        # print(' '.join(left.text.split()), ' '.join(right.text.split()), sep='\n')
+        wwww =[' '.join(left.text.split()),' '.join(right.text.split())]
+    return wwww
 
 # parser_station('minsk','autobus','69')
 
 
-def parser_all_station(city, transport, number_transport):
-    url = f'https://kogda.by/routes/{city}/{transport}/{number_transport}'
-    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-    html = r.text
-    soup = BeautifulSoup(html, 'lxml')
-    quote_boxes = soup.find('ul', {"class": "list-group"}).text
-    quote_boxes1 = soup.find('div', {"id": "direction-1"}).text
-
-    return print(','.join(quote_boxes.split()),','.join(quote_boxes1.split()), sep='\n \n')
-
-parser_all_station('minsk','autobus','69')
+# def parser_all_station(city, transport, number_transport):
+#     url = f'https://kogda.by/routes/{city}/{transport}/{number_transport}'
+#     r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+#     html = r.text
+#     soup = BeautifulSoup(html, 'lxml')
+#     quote_boxes = soup.find('ul', {"class": "list-group"}).text
+#     quote_boxes1 = soup.find('div', {"id": "direction-1"}).text
+#
+#     return print(','.join(quote_boxes.split()),','.join(quote_boxes1.split()), sep='\n \n')
+#
+# parser_all_station('minsk','autobus','69')
 
 
