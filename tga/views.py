aa9aa3c03690876a1cc2d6d886b7f5224df4bf7a
Salavei <kano.mori@mail.ru>
2021-11-26 14:08:00 +0300

    add mobil settings css file. Fix view.py(change money grab information(defore APi. After Parse)

--- tga/views.py
+++ tga/views.py
@@ -1,19 +1,41 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import requests
+from bs4 import BeautifulSoup
 
-api = 'f89fed0d4d207b879a2d'
+byn = 'https://clck.ru/Z4Ay4'
+rub = 'https://clck.ru/Z4Ax3'
+usd = 'https://clck.ru/Z4Avo'
+bun_r = requests.get(byn, headers={
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.2 Safari/605.1.15',
+    'Accept-Language': 'ru',
+})
+rub_r = requests.get(rub, headers={
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.2 Safari/605.1.15',
+    'Accept-Language': 'ru',
+})
+usd_r = requests.get(usd, headers={
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.2 Safari/605.1.15',
+    'Accept-Language': 'ru',
+})
+html_bun = bun_r.text
+html_rub = rub_r.text
+html_usd = usd_r.text
 
-byn = requests.get(f'https://free.currconv.com/api/v7/convert?q=UAH_BYN&compact=ultra&apiKey={api}')
-rub = requests.get(f'https://free.currconv.com/api/v7/convert?q=UAH_RUB&compact=ultra&apiKey={api}')
-usd = requests.get(f'https://free.currconv.com/api/v7/convert?q=UAH_USD&compact=ultra&apiKey={api}')
+soup = BeautifulSoup(html_bun, 'lxml')
+take_for_was_and_now = soup.find_all('div', {"class": "main"})
+clear_bun = soup.find('span', {"class": "DFlfde SwHCTb"}).contents[0]
 
+soup = BeautifulSoup(html_rub, 'lxml')
+take_for_was_and_now1 = soup.find_all('div', {"class": "main"})
+clear_rub = soup.find('span', {"class": "DFlfde SwHCTb"}).contents[0]
+
+soup = BeautifulSoup(html_usd, 'lxml')
+take_for_was_and_now2 = soup.find_all('div', {"class": "main"})
+clear_usd = soup.find('span', {"class": "DFlfde SwHCTb"}).contents[0]
 
 def first_page(request):
-    info_bun = byn.json()['UAH_BYN']
-    info_rub = rub.json()['UAH_RUB']
-    info_usd = usd.json()['UAH_USD']
-    return render(request, './main.html', {'byn': (int((info_bun * 40) * 100) / 100),
-                                           'rub': (int((info_rub * 40) * 100) / 100),
-                                           'usd': (int((info_usd * 40) * 100) / 100)
+    return render(request, './main.html', {'byn': clear_bun,
+                                           'rub': clear_rub,
+                                           'usd': clear_usd,
                                            })
